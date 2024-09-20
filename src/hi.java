bfcad364cac0eb3e0dc545428a3b7ff5a68c9f7f
nishan733689 <nishan733689ny@gmail.com>
2024-09-20 19:04:14 +0600

    hi

--- src/hi.java
+++ src/hi.java
@@ -1,5 +1,5 @@
 public class hi {
-    public static void main(String[] srgs){
-        System.out.println("I like banana");
+    public static void main(String[] args){
+        System.out.println("I like banana "+"mango");
     }
 }
